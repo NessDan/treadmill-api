7887a1a24c458d0d71d2d47955bc3c56aee45e5e
Daniel Kaspo <nessdan@users.noreply.github.com>
2018-08-11 22:48:50 -0400

    Make them post

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const treadmill = require('./treadmill.js');
 const app = express();
 
-app.get(`/start`, treadmill.start);
-app.get(`/stop`, treadmill.stop);
-app.get(`/setSpeed`, treadmill.setSpeed);
-app.get(`/inclineUp`, treadmill.increaseIncline);
-app.get(`/inclineDown`, treadmill.decreaseIncline);
+app.post(`/start`, treadmill.start);
+app.post(`/stop`, treadmill.stop);
+app.post(`/setSpeed`, treadmill.setSpeed);
+app.post(`/inclineUp`, treadmill.increaseIncline);
+app.post(`/inclineDown`, treadmill.decreaseIncline);
 app.listen(3000, () => console.log(`Example app listening on port 3000!`));
