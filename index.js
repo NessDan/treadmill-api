91391901899730e4bc03cc5d09cd0f7c1a1241f1
Daniel Kaspo <nessdan@users.noreply.github.com>
2018-09-03 14:49:59 -0400

    Updated api endpoint usage to be inline with spec

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require('express');
 const apiRouting = require('./routing/api.js');
 const app = express();
 
-app.post(`/api`, apiRouting);
+app.use(`/api`, apiRouting);
 app.listen(3000, () => console.log(`Example app listening on port 3000!`));
