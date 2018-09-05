69a961cd3774e9a80e2c2bc3e74679c669336c5f
Daniel Kaspo <nessdan@users.noreply.github.com>
2018-09-05 00:13:22 -0400

    Removed un-needed log messages

--- routing/api.js
+++ routing/api.js
@@ -6,61 +6,43 @@ const Decimal = require('decimal.js');
 // All of these functions can be hit by visiting /api/functionName
 const routing = {
     setSpeed: (req, res) => {
-        console.log(req.query);
-        let logMessage = `Could not set speed. Must pass "mph" parameter.`;
-
         if (req.query && req.query.mph) {
             const mph = new Decimal(req.query.mph);
-            logMessage = `Setting speed to ${mph.toFixed(1)}mph`;
 
-            console.log(logMessage);
             treadmill.setSpeed(mph);
-            res.send(logMessage);
+            res.send(mph);
         } else {
-            console.log(logMessage);
-            res.send(logMessage);
+            res.send(mph);
         }
     },
     getSpeed: (_req, res) => {
-        const treadmillSpeed = treadmill.currentSpeed.toFixed(1);
-        const logMessage = `Returning treadmill speed of ${treadmillSpeed}mph`;
-        console.log(logMessage);
+        const treadmillSpeed = treadmill.getSpeed();
         res.send(treadmillSpeed);
     },
     setIncline: (req, res) => {
-        console.log(req.query);
-        let logMessage = `Could not set incline. Must pass "grade" parameter.`;
-
         if (req.query && req.query.grade) {
             const grade = new Decimal(req.query.grade);
-            logMessage = `Setting incline to ${grade.toFixed(1)}% grade`;
 
-            console.log(logMessage);
             treadmill.goToIncline(grade);
-            res.send(logMessage);
+            res.send(grade);
         } else {
-            console.log(logMessage);
-            res.send(logMessage);
+            res.send(grade);
         }
     },
     getIncline: (_req, res) => {
         const treadmillIncline = treadmill.currentIncline.toFixed(1);
-        const logMessage = `Returning treadmill incline of ${treadmillIncline}% grade`;
-        console.log(logMessage);
         res.send(treadmillIncline);
     },
     calibrateIncline: (_req, res) => {
-        const logMessage = `Calibrating incline...`;
-        console.log(logMessage);
         treadmill.calibrateIncline();
-        res.send(treadmillIncline);
+        res.send();
     },
 };
 
 const loggerMiddleware = (req, _res, next) => {
     const logMessage = {
         date: Date.now(),
-        endpoint: req.originalUrl,
+        endpoint: req.path,
         params: req.params,
         query: req.query,
     };
