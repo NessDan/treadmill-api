0c801b40db4d2eb0331238e9f824642fcb6c8439
NessDan <nessdan@users.noreply.github.com>
2018-10-02 18:33:45 -0400

    Added SSL support for treadmill app.

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const apiRouting = require('./routing/api.js');
 const app = express();
+const https = require('https');
+const fs = require('fs');
 
+app.use(express.static('static')); // For LetsEncrypt
+app.use(require('helmet')()); // For LetsEncrypt / Turning on SSL
 app.use(`/api`, apiRouting);
-app.listen(3000, () => console.log(`Example app listening on port 3000!`));
+//app.listen(80, () => console.log(`Treadmill API now listening.`));
+
+const options = {
+    cert: fs.readFileSync('/etc/letsencrypt/live/treadmill.nessdan.net/fullchain.pem'),
+    key: fs.readFileSync('/etc/letsencrypt/live/treadmill.nessdan.net/privkey.pem'),
+};
+
+https.createServer(options, app).listen(443);
+console.log(`Treadmill API now listening on SSL.`);
