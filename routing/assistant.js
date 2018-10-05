a17ea300582c28935748297b5c8921c819c8c55a
Daniel Kaspo <nessdan@users.noreply.github.com>
2018-10-04 20:15:29 -0400

    Formatting

--- routing/assistant.js
+++ routing/assistant.js
@@ -4,29 +4,29 @@ const assistantRouter = express.Router();
 
 // All of these functions can be hit by visiting /api/functionName
 const routing = {
-    routeAssistantWebhook: (req, res) => {
-        if (req && req.body && req.body.queryResult && req.body.queryResult.action) {
-            if (req.body.queryResult.action === 'treadmill.start') {
-                treadmill.setSpeed(1);
-            }
-        }
-        res.json({
-            "payload": {
-              "google": {
-                "expectUserResponse": true,
-                "richResponse": {
-                  "items": [
-                    {
-                      "simpleResponse": {
-                        "textToSpeech": "Done. How you livin', son?"
-                      }
-                    }
-                  ]
+  routeAssistantWebhook: (req, res) => {
+    if (req && req.body && req.body.queryResult && req.body.queryResult.action) {
+      if (req.body.queryResult.action === 'treadmill.start') {
+        treadmill.setSpeed(1);
+      }
+    }
+    res.json({
+      "payload": {
+        "google": {
+          "expectUserResponse": true,
+          "richResponse": {
+            "items": [
+              {
+                "simpleResponse": {
+                  "textToSpeech": "Done. How you livin', son?"
                 }
               }
-            }
-          });
-    },
+            ]
+          }
+        }
+      }
+    });
+  },
 };
 
 assistantRouter.post('/', routing.routeAssistantWebhook);
