453f57cd9b9aad0ffea7c69ed996b1788b97d0e6
Daniel Kaspo <nessdan@users.noreply.github.com>
2018-10-13 15:14:00 -0400

    Fixing a syntax issue

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const apiRouting = require('./routing/api.js');
 const assistantRouting = require('./routing/assistant.js');
 const routeLogger = require('./routing/logger.js');
-const basicAuth = require('express-basic-auth')
+const basicAuth = require('express-basic-auth');
 const app = express();
 const https = require('https');
 const fs = require('fs');
@@ -14,11 +14,11 @@ app.use(express.json());
 app.use(express.static('static')); // For LetsEncrypt
 app.use(require('helmet')()); // For LetsEncrypt / Turning on SSL
 if (password) {
-    app.use(basicAuth, {
+    app.use(basicAuth({
         users: {
             'admin': password,
         }
-    });
+    }));
 }
 app.use(routeLogger); // Logs out to STDOUT with useful request info.
 app.use(`/api`, apiRouting);
