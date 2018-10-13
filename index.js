69a892e5a5024154ac8288e477c28f2812ebdbfc
Daniel Kaspo <nessdan@users.noreply.github.com>
2018-10-13 15:09:16 -0400

    Adding basic auth

--- index.js
+++ index.js
@@ -2,13 +2,24 @@ const express = require('express');
 const apiRouting = require('./routing/api.js');
 const assistantRouting = require('./routing/assistant.js');
 const routeLogger = require('./routing/logger.js');
+const basicAuth = require('express-basic-auth')
 const app = express();
 const https = require('https');
 const fs = require('fs');
 
+// Load up the authorization file
+const password = fs.readFileSync('password');
+
 app.use(express.json());
 app.use(express.static('static')); // For LetsEncrypt
 app.use(require('helmet')()); // For LetsEncrypt / Turning on SSL
+if (password) {
+    app.use(basicAuth, {
+        users: {
+            'admin': password,
+        }
+    });
+}
 app.use(routeLogger); // Logs out to STDOUT with useful request info.
 app.use(`/api`, apiRouting);
 app.use(`/assistant`, assistantRouting);
