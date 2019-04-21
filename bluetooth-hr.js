3a9711acea7a124d59fdf94665f08331109cc8a9
NessDan <nessdan@users.noreply.github.com>
2019-04-21 00:42:12 -0400

    Can find HR bluetooth devices.

--- bluetooth-hr.js
+++ bluetooth-hr.js
@@ -1,1 +1,7 @@
-const noble = require('noble');
+//const noble = require('noble');
+const noble = require('@abandonware/noble');
+
+noble.on('discover', (per) => { console.log(per);});
+
+noble.startScanning(["180d"]); // HR services only
+//noble.startScanning(); // any service UUID, no duplicates
