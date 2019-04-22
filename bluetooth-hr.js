395bc0a7954ef4be283771f0482c23df17a47660
Daniel Kaspo <nessdan@users.noreply.github.com>
2019-04-21 22:55:53 -0400

    Make sure to wait for conneciton b efore proceeding.

--- bluetooth-hr.js
+++ bluetooth-hr.js
@@ -2,22 +2,31 @@
 const noble = require('@abandonware/noble');
 
 noble.on('discover', (peripheral) => {
+    noble.on('scanStop', () => {
+        peripheral.once('connect', () => {
+            // peripheral.discoverServices(["180d"], (error, services) => {
+            peripheral.discoverAllServicesAndCharacteristics((error, services, characteristics) => {
+                if (error) {
+                    console.log('err2', error);
+                }
+
+                console.log(services);
+                console.log(characteristics);
+            });
+
+        });
+
+        peripheral.connect((error) => {
+            if (error) {
+                console.log('error');
+                console.log(error);
+            }
+        });
+
+    });
+
     console.log(peripheral);
     noble.stopScanning();
-
-    peripheral.connect((error) => {
-        console.log('error');
-        console.log(error);
-    });
-    // peripheral.discoverServices(["180d"], (error, services) => {
-    peripheral.discoverAllServicesAndCharacteristics((error, services, characteristics) => {
-        if (error) {
-            console.log('err2', error);
-        }
-
-        console.log(services);
-        console.log(characteristics);
-    });
 });
 
 noble.startScanning(["180d"]); // HR services only
