8fb87a3cd80c2273e776345d0550e41d197f5b18
Daniel Kaspo <nessdan@users.noreply.github.com>
2019-04-21 22:27:38 -0400

    More testing code for peripherals.

--- bluetooth-hr.js
+++ bluetooth-hr.js
@@ -1,7 +1,22 @@
 //const noble = require('noble');
 const noble = require('@abandonware/noble');
 
-noble.on('discover', (per) => { console.log(per);});
+noble.on('discover', (per) => {
+    console.log(per);
+    noble.stopScanning();
+
+    peripheral.connect((error) => {
+        console.log('error');
+        console.log(error);
+    });
+    peripheral.discoverServices(["180d"], (error, services) => {
+        if (error) {
+            console.log('err2', error);
+        }
+
+        console.log(services);
+    });
+});
 
 noble.startScanning(["180d"]); // HR services only
 //noble.startScanning(); // any service UUID, no duplicates
