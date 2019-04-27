687bcbdd31ecfead8e872e41f8edf6d0a83ad6c1
Daniel Kaspo <nessdan@users.noreply.github.com>
2019-04-26 21:41:58 -0400

    Trying something again

--- treadmill/heart/miband.js
+++ treadmill/heart/miband.js
@@ -59,10 +59,7 @@ const treadmill = {
     authChar.write(new Buffer.from("0208", "hex"), true);
   },
   sendEncryptedKey: (encrypedKey, authChar) => {
-    authChar.write(
-      new ArrayBuffer(Buffer.from("0308" + encrypedKey, "hex").length),
-      true
-    );
+    authChar.write(new Buffer.from("0108" + encrypedKey, "hex"), true);
   },
   handleAuthResponse: (response, authChar) => {
     const response2 = new Buffer.from(response);
