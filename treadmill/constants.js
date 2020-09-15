4a2aa0dad97afd5a2a229f2b687e6495464f6d4a
Daniel Kaspo <304269+NessDan@users.noreply.github.com>
2020-09-15 12:42:40 -0400

    Update max speed to 6

--- treadmill/constants.js
+++ treadmill/constants.js
@@ -1,7 +1,7 @@
 const Decimal = require("decimal.js");
 
 const constants = {
-  maxSpeed: new Decimal(4),
+  maxSpeed: new Decimal(6),
   startSpeed: new Decimal(2),
   updateLogicLoopInterval: 100, // Update speed / incline every 100ms
   speedWireFrequency: 20, // Treadmill uses 20Hz freq from testing.
