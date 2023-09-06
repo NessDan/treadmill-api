908cede2ec8c4ba984956adde368c522d9ac124b
NessDan <NessDan@users.noreply.github.com>
2023-09-06 12:18:09 -0400

    remove letsencrypt / helmet / https

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const assistantRouting = require("./routing/assistant.js");
 const routeLogger = require("./routing/logger.js");
 const basicAuth = require("express-basic-auth");
 const app = express();
-const letsEncryptApp = express();
-const https = require("https");
 const fs = require("fs");
 const cors = require("cors");
 
@@ -14,8 +12,6 @@ const password = fs.readFileSync("password", "utf8").trim();
 
 app.use(cors());
 app.use(express.json());
-letsEncryptApp.use(express.static("static")); // For LetsEncrypt
-app.use(require("helmet")()); // For LetsEncrypt / Turning on SSL
 if (password) {
   app.use(
     basicAuth({
@@ -30,18 +26,6 @@ app.use(express.static("web"));
 app.use(`/api`, apiRouting);
 app.use(`/assistant`, assistantRouting);
 
-const options = {
-  cert: fs.readFileSync(
-    "/etc/letsencrypt/live/treadmill.nessdan.net/fullchain.pem"
-  ),
-  key: fs.readFileSync(
-    "/etc/letsencrypt/live/treadmill.nessdan.net/privkey.pem"
-  ),
-};
-
-// letsEncryptApp.listen(80); // Commented out when switching to locally hosted only
 app.listen(80, () => {
   console.log(`Treadmill API now listening.`);
 });
-https.createServer(options, app).listen(443);
-console.log(`Treadmill API now listening.`);
